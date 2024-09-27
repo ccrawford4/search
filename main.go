73f939219a0dd15b77d6e67a1e0d4306392f67fa
ccrawford4 <calumcrawford9@gmail.com>
2024-09-26 21:48:39 -0700

    started api

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
-import (
-	"time"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	go startServer("8080") // For serving the contents of the web page
-
 	index := make(Index)
+	router := gin.Default()
 	stopWords := getStopWords()
-	initHandlers(&index, stopWords)
-	crawl(&index, parseURL("http://127.0.0.1:8080/documents/top11/"), stopWords)
-	// Loop indefinitely
-	for {
-		time.Sleep(100 * time.Millisecond)
-	}
+	router.GET("/search", func(c *gin.Context) {
+		term, found := c.Params.Get("term")
+		if !found {
+			c.JSON(400, gin.H{})
+		}
+		// get the searchTerm from the Request and then search the index for the term
+		searchTerm := getStemmedWord(term, stopWords)
+		freq, found := search(&index, searchTerm, stopWords)
+
+		// If no search term was found then serve the no_results document
+		if !found {
+			c.JSON(401, gin.H{})
+			return
+		}
+
+		// Convert the Frequency found into templateData to be embedded into the html
+		templateData := getTemplateData(freq, searchTerm)
+		// fileContent, _ := openAndReadFile("./static/search.html")
+		// executeTemplate(*w, string(fileContent), templateData)
+		c.IndentedJSON(200, templateData)
+	})
+	go router.Run(":8080")
+	crawl(&index, parseURL("https://cs272-f24.github.io/top10/"), stopWords)
 }
