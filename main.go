54687f7265b9178ba3aae581f819b0ba770ee302
Calum Crawford <123438979+ccrawford4@users.noreply.github.com>
2024-10-09 16:21:18 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,61 +1,29 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
+	_ "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	_ "github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	_ "net/http"
 	"os"
-	"time"
 )
 
-func init() {
-	// Only load .env file in development environment
-	if os.Getenv("ENV") == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalf("Error loading .env file: %v\n", err)
-		}
-	}
-}
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	connString, exists := os.LookupEnv("AZURE_SQL_CONNECTIONSTRING")
 	if !exists {
 		log.Fatalf("No Connection AZURE_SQL_CONNECTIONSTRING Provided\n")
 	}
-	log.Printf("Connecting string loaded successfully: %v\n", connString)
-	redisHost, exists := os.LookupEnv("REDIS_HOST")
-	if !exists {
-		log.Fatalf("No Redis Host Provided\n")
-	}
-	redisPassword, exists := os.LookupEnv("REDIS_PASSWORD")
-	if !exists {
-		log.Fatalf("No Redis Password Provided\n")
-	}
-	rsClient, err := getRSClient(redisHost, redisPassword)
-	if err != nil {
-		log.Fatalf("Error getting Redis Client: %v\n", err)
-	}
 
 	var idx Index
-	idx = newDBIndex(connString, false, rsClient)
+	idx = newDBIndex(connString, false)
 	router := gin.Default()
-	if os.Getenv("ENV") == "development" {
-		router.Use(cors.New(cors.Config{
-			AllowOrigins:     []string{"http://127.0.0.1:3000", "http://localhost:3000"},
-			AllowMethods:     []string{"POST", "OPTIONS"},
-			AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Add any other required headers here
-			ExposeHeaders:    []string{"Content-Length"},
-			AllowCredentials: true,
-			MaxAge:           12 * time.Hour,
-		}))
-	}
-
-	router.GET("/documents/top10/*any", func(c *gin.Context) {
-		corpusHandler(c.Writer, c.Request)
-	})
 
 	router.POST("/search", func(c *gin.Context) {
 		type SearchRequestBody struct {
@@ -69,18 +37,16 @@ func main() {
 
 		// get the searchTerm from the Request and then search the index for the term
 		result := getTemplateData(&idx, searchRequestBody.SearchTerm)
-		if result == nil {
-			c.IndentedJSON(404, gin.H{"error": "No results found"})
-		} else {
-			c.IndentedJSON(200, result)
-		}
+		c.IndentedJSON(200, result)
 	})
 
-	// Use the test crawl flag to avoid parsing robots.txt and delaying overall crawl time
-	// go crawl(&idx, "https://usfca.edu/", false)
+	url, err := parseURL("https://cs272-f24.github.io/top10/")
+	if err != nil {
+		log.Fatalf("Could not parse seed url: %v", err)
+	}
+	go crawl(&idx, url)
 	err = router.Run(":8080")
 	if err != nil {
 		return
 	}
-
 }
