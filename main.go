6b2f73fb322b7f3267d057d7c0726f4bce828b23
ccrawford4 <calumcrawford9@gmail.com>
2024-10-09 16:03:34 -0700

    change to use local

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-contrib/cors"
 	_ "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
@@ -18,47 +16,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	server, exists := os.LookupEnv("server")
+	connString, exists := os.LookupEnv("AZURE_SQL_CONNECTIONSTRING")
 	if !exists {
-		log.Fatalf("Error! Enviornment variable 'server' does not exist")
-	}
-	user, exists := os.LookupEnv("user")
-	if !exists {
-		log.Fatalf("Error! Enviornment variable 'user' does not exist")
-	}
-	password, exists := os.LookupEnv("password")
-	if !exists {
-		log.Fatalf("Error! Environment variable 'password' does not exist")
-	}
-	port, exists := os.LookupEnv("port")
-	if !exists {
-		log.Fatalf("Error! Enviornment variable 'port' does not exist")
-	}
-	database, exists := os.LookupEnv("database")
-	if !exists {
-		log.Fatalf("Error! Enviornment variable 'database' does not exist")
-	}
-	api, exists := os.LookupEnv("api")
-	if !exists {
-		log.Fatalf("Error! Environment variable 'api' does not exist")
+		log.Fatalf("No Connection AZURE_SQL_CONNECTIONSTRING Provided\n")
 	}
 
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=%s;",
-		server, user, password, port, database)
-
-	// playground(connString)
-
-	fmt.Printf("connString: %v\n", connString)
 	var idx Index
 	idx = newDBIndex(connString, false)
 	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{api},                      // Allow requests from this origin
-		AllowMethods:     []string{"GET", "POST", "OPTIONS"}, // Allow these methods
-		AllowHeaders:     []string{"Content-Type"},           // Allow these headers
-		AllowCredentials: true,                               // Allow cookies or other credentials
-	}))
 
 	router.POST("/search", func(c *gin.Context) {
 		type SearchRequestBody struct {
